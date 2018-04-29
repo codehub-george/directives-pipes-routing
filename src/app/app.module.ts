230aba5b4a92659f2fa12fe1d25a6ee027388ffb
George <geor.oikonomopoulos@gmail.com>
2018-04-30 01:54:23 +0300

    routing vol.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ListComponentComponent } from './list-component/list-component.component';
+import { SomeComponentComponent } from './some-component/some-component.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { routes } from './routes';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ListComponentComponent,
+    SomeComponentComponent,
+    DashboardComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
