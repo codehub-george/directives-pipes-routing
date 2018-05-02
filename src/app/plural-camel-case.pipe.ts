3bfd912b8a0a8580efdfa81262032484a0740f6e
codehub-george <38844446+codehub-george@users.noreply.github.com>
2018-05-02 11:50:24 +0300

    Update and rename plural-camel-case.pipe.ts to capitalize.pipe.ts

--- src/app/plural-camel-case.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'pluralcamelcase' })
-export class PluralCamelCase implements PipeTransform {
-	transform(value: string): string {
-		let lowercaseValue = value.toLowerCase();
-		return lowercaseValue.substr(0, 1).toUpperCase() + lowercaseValue.substr(1) + 's';
-	}
-}
